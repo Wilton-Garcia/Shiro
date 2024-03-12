fa33e091f77e6a2f22c7915db87e1ae8d6822f84
Wilton Garcia <wiltongarciaweb@gmail.com>
2024-03-12 16:14:14 -0300

    Creating Project

---
+++ Shiro/ShiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiroApp.swift
+//  Shiro
+//
+//  Created by Wilton Garcia on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
